c385f964fc53bea63656203b534a09606b89447d
MuhamadArfan16 <muhammadarfan1600@gmail.com>
2023-03-19 08:11:12 +0700

    first commit

---
+++ helpers/init.go
@@ -0,0 +1,15 @@
+package helpers
+
+func Init() []student {
+	data_student := []student{
+		{},
+		{nama: "Chandra Prasetyo", alamat: "DKI Jakarta", pekerjaan: "Telemarketing", alasan: "Meningkatkan skill dibidang golang"},
+		{nama: "Gideon Tri NMulyanto", alamat: "Jawa Barat", pekerjaan: "Sales", alasan: "Meningkatkan skill dibidang golang 2"},
+		{nama: "Gian Felix Ramadan", alamat: "Yogyakarta", pekerjaan: "IT Support", alasan: "Meningkatkan skill dibidang golang 3"},
+		{nama: "Agung Syahputra", alamat: "Sumatera Selatan", pekerjaan: "Admin", alasan: "Meningkatkan skill dibidang golang 4"},
+		{nama: "Febrianto", alamat: "Jawa Barat", pekerjaan: "Receptionis", alasan: "Meningkatkan skill dibidang golang 5"},
+		{nama: "Haidar Nashiruddin", alamat: "Jawa Barat", pekerjaan: "Inspektur", alasan: "Meningkatkan skill dibidang golang 6"},
+		{nama: "Moh Iman Wahyudi", alamat: "Jawa Timur", pekerjaan: "Auditor", alasan: "Meningkatkan skill dibidang golang 7"},
+	}
+	return data_student
+}
